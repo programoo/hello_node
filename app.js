bb42e69c6dbdb8b455d2eba88f2d6d9e5e363533
programoo <prakarnwongsanit@gmail.com>
2020-04-11 12:42:07 +0700

    First commit

---
+++ app.js
@@ -0,0 +1,25 @@
+console.log("Hi Node");
+
+const https = require("https");
+const request = https.get("https://teamtreehouse.com/chalkers.json", response => {
+    let body  = "";
+
+    response.on("data", dataChunk => {
+        body += dataChunk;
+        console.log(dataChunk);
+    });
+
+    response.on("end", () => {
+        const profile = JSON.parse(body);
+
+        console.log(profile.points.JavaScript);
+        // console.dir(profile);
+    });
+
+});
+
+request.on("error", error => {
+    console.error(error.message);
+});
+
+request
